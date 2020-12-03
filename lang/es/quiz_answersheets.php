5625d50e8ad1e62f345b4b3a188ab25e5d7fc8ff
Juan Eladio Sánchez Rosas <juaneladio.sanchez@gmail.com>
2020-12-02 19:08:47 -0500

    New translation: Spanish (es)

--- lang/es/quiz_answersheets.php
+++ lang/es/quiz_answersheets.php
@@ -22,95 +22,95 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Export quiz attempts';
-$string['answersheets'] = 'Export attempts';
-$string['answersheetsfilename'] = 'Exportable_quiz_attempts';
-$string['answersheetsreport'] = 'Export quiz attempts';
-$string['admin_instruction_message'] = 'Instruction message';
-$string['admin_instruction_message_des'] = 'If set, this text will be shown at the top of the report. You can use this, for example, to link to any institutional policies about printing summative quizzes.';
-$string['answersheets:bulkdownload'] = 'Download review sheet in bulk';
-$string['answersheets:componentname'] = 'Export quiz attempts';
-$string['answersheets:createattempt'] = 'Create an attempt for another user';
-$string['answersheets:submitresponses'] = 'Submit student responses';
-$string['answersheets:view'] = 'View attempt sheet';
-$string['answersheets:viewrightanswers'] = 'View right answer sheet';
-$string['answer_sheet_label'] = 'Right answer sheet';
-$string['attempt_sheet_label'] = 'Attempt sheet';
-$string['answer_sheet_title'] = '{$a->courseshortname} - {$a->quizname} - Answer sheet';
-$string['attempt_sheet_title'] = '{$a->courseshortname} - {$a->quizname} - Attempt sheet';
-$string['bulkdownloadlink'] = 'Download review sheets in bulk';
-$string['bulkinstructions'] = 'To be able to download review sheets in bulk, you need the
-`save-answersheets` tool on your computer. Once you have that:
+$string['pluginname'] = 'Exportar intentos de cuestionarios';
+$string['answersheets'] = 'Exportar intentos';
+$string['answersheetsfilename'] = 'Intentos_de_cuestionario_exportados';
+$string['answersheetsreport'] = 'Exportar intentos de cuestionarios';
+$string['admin_instruction_message'] = 'Mensaje de instrucciones';
+$string['admin_instruction_message_des'] = 'Si se escribe aquí, este texto será mostrado al inicio del reporte. Puedes usarlo para, por ejemplo, para enlazar a cualquier política institucional acerca de la impresión de cuestionarios sumativos.';
+$string['answersheets:bulkdownload'] = 'Descargar todas las hojas de revisión en bloque';
+$string['answersheets:componentname'] = 'Exportar intentos de cuestionario';
+$string['answersheets:createattempt'] = 'Crear un intento para otro usuario';
+$string['answersheets:submitresponses'] = 'Enviar respuestas de estudiante';
+$string['answersheets:view'] = 'Ver hoja del intento (sin marcar)';
+$string['answersheets:viewrightanswers'] = 'Ver hoja con respuestas';
+$string['answer_sheet_label'] = 'Hoja con respuestas';
+$string['attempt_sheet_label'] = 'Hoja del intento (sin marcar)';
+$string['answer_sheet_title'] = '{$a->courseshortname} - {$a->quizname} - Hoja con respuestas';
+$string['attempt_sheet_title'] = '{$a->courseshortname} - {$a->quizname} - Hoja del intento';
+$string['bulkdownloadlink'] = 'Descargar todas las hojas de revisión en bloque';
+$string['bulkinstructions'] = 'Para descargar todas las hojas de revisión en bloque, necesitarás la herramienta
+`save-answersheets` en tu computadora. Una vez que la tengas:
 
-1. The attempts that will be downloaded when you follow these instructions are based on the settings of the report you just left.
-   What this process will do is effectively follow every **Review sheet** link there. So, if you are in any doubt, go back and
-   check the report is showing the attempts you want exported.
-2. Once you are satisfied, download the [bulk download steps file]({$a->scripturl}) that will tell `save-answersheets` what to do.
-   **Don\'t forget the warning above!**
-3. Save that file (which should be called `{$a->scriptname}-steps.txt`) in the same folder where you have `save-answersheets`
-   on your computer.
-4. Open a command prompt and go to that folder.
-5. Type the command `.\save-answersheets {$a->scriptname}-steps.txt` and wait for it to run. It outputs what it is doing as it goes.
-6. Once the script has finished, you should have a file `{$a->scriptname}.zip` inside the `output` folder.
-7. Remember to delete the `{$a->scriptname}-steps.txt` file.';
-$string['bulkinstructionstitle'] = 'Instructions for downloading review sheets in bulk';
-$string['bulkinstructionswarning'] = '<b>Warning</b>! the file you download in Step 2 of the instructions below contains enough
-information for the tool to access the quiz attempts to be saved using your current login session. You <b>must</b> delete
-that file as soon as you have finished with it. Retaining it is a security risk.';
-$string['column_answer_sheet'] = 'Answer sheets';
-$string['column_attempt_sheet'] = 'Attempt sheets';
-$string['column_submit_student_responses'] = 'Submit student responses';
-$string['create_attempt'] = 'Create Attempt';
-$string['create_attempt_modal_button'] = 'Create';
-$string['create_attempt_modal_description'] = 'Are you sure you want to create a quiz attempt for {$a}?';
-$string['create_attempt_modal_title'] = 'Confirmation';
-$string['event_attempt_created'] = 'Quiz attempt created for user';
-$string['event_attempt_viewed'] = 'Quiz attempt sheet viewed';
-$string['event_attempt_printed'] = 'Quiz attempt sheet printed';
-$string['event_right_answer_viewed'] = 'Quiz right answer sheet viewed';
-$string['event_right_answer_printed'] = 'Quiz right answer sheet printed';
-$string['event_responses_submitted'] = 'Quiz responses submitted for user';
-$string['examcode'] = 'Confirmation code';
-$string['interactive_content_warning'] = 'Interactive content is not available in this format.';
-$string['page_type_attempt'] = 'Attempt sheet';
-$string['page_type_review'] = 'Review sheet';
-$string['page_type_answer'] = 'Answer sheet';
-$string['print'] = 'Print';
-$string['privacy:metadata'] = 'The Export quiz attempts plugin does not store any personal data itself. It provides an additional interface for viewing and managing the data owned by the quiz activity.';
-$string['print_header'] = '{$a->courseshortname} {$a->quizname} for {$a->studentname} generated {$a->generatedtime} - {$a->sheettype}';
-$string['print_header_minimised'] = '{$a->courseshortname} {$a->quizname} generated {$a->generatedtime} - {$a->sheettype}';
-$string['review_sheet_label'] = 'Review sheet';
-$string['review_sheet_title'] = '{$a->courseshortname} - {$a->quizname} - Review sheet';
-$string['showuserinfo'] = 'Identifying information to show about users';
-$string['submit_student_responses_label'] = 'Submit responses...';
-$string['submit_student_responses_on_behalf'] = 'Submit responses on behalf of {$a} and finish attempt';
-$string['submit_student_responses_dialog_content'] = 'Are you sure you want to submit?';
-$string['submit_student_responses_title'] = '{$a}: Submit student responses';
+1. Los intentos que serán descargados cuando sigas estas instrucciones están basados en los parámetros del reporte que dejaste en la página anterior.
+   Lo que este proceso hará será seguir cada enlace de **Hoja de revisión** que aparezca en él. En caso de duda, retrocede y verifica que
+   el reporte está mostrando los intentos que quieres exportar.
+2. Una vez que estés de acuerdo, descarga el [archivo de pasos de descarga masiva]({$a->scripturl}) que le dirá al programa `save-answersheets` lo que debe hacer.
+   **¡No olvides la advertencia de arriba!**
+3. Descarga ese archivo (que se llamará`{$a->scriptname}-steps.txt`) en la misma carpeta que tienes el programa `save-answersheets`
+   en tu computadora.
+4. Abre una línea de comando (cmd / Símbolo de sistema) y ve a la carpeta donde está el programa y el archivo.
+5. Escribe el comando `.\save-answersheets {$a->scriptname}-steps.txt` y espera su ejecución. Mostrará lo que está haciendo y terminará de ejecutarse.
+6. Una vez que la ejecución del script termine, debes tener un archivo `{$a->scriptname}.zip` dentro de la carpeta `output`.
+7. No olvides borrar el archivo `{$a->scriptname}-steps.txt`.';
+$string['bulkinstructionstitle'] = 'Instrucciones para descargar todas las hojas de revisión en bloque';
+$string['bulkinstructionswarning'] = '¡<b>Advertencia</b>! el archivo que descargaste en el Paso 2 de las instrucciones que vienen a continuación contiene suficiente 
+información para la herramienta para acceder a los intentos de cuestionarios que serán descargados usando tu sesión actual. Tu <b>debes</b> eliminar
+ese archivo tan pronto como termines con la descarga. Mantenerlo es un riesgo de seguridad.';
+$string['column_answer_sheet'] = 'Hojas de respuestas';
+$string['column_attempt_sheet'] = 'Hojas de intentos';
+$string['column_submit_student_responses'] = 'Enviar respuestas de estudiante';
+$string['create_attempt'] = 'Crear intento';
+$string['create_attempt_modal_button'] = 'Crear';
+$string['create_attempt_modal_description'] = 'Estás seguro de que deseas crear un intento de cuestionario para {$a}?';
+$string['create_attempt_modal_title'] = 'Confirmación';
+$string['event_attempt_created'] = 'Intento de cuestionario creado para el usuario';
+$string['event_attempt_viewed'] = 'Hoja de intento de cuestionario visualizada';
+$string['event_attempt_printed'] = 'Hoja de intento de cuestionario impresa';
+$string['event_right_answer_viewed'] = 'Hoja de respuestas de cuestionario visualizada';
+$string['event_right_answer_printed'] = 'Hoja de intentos de cuestionario impresa';
+$string['event_responses_submitted'] = 'Respuestas de cuestionario enviadas por el usuario';
+$string['examcode'] = 'Código de confirmación';
+$string['interactive_content_warning'] = 'El contenido interactivo no está disponible en este formato.';
+$string['page_type_attempt'] = 'Hoja de intento';
+$string['page_type_review'] = 'Hoja de revisión';
+$string['page_type_answer'] = 'Hoja de respuesta';
+$string['print'] = 'Imprimir';
+$string['privacy:metadata'] = 'El complemento de Exportar intento de cuestionarios no almacena información personal por sí mismo. Proporciona una interfaz adicional para ver y administrar los datos pertenecientes a la actividad de cuestionario.';
+$string['print_header'] = '{$a->courseshortname} {$a->quizname} para {$a->studentname} generado {$a->generatedtime} - {$a->sheettype}';
+$string['print_header_minimised'] = '{$a->courseshortname} {$a->quizname} generado {$a->generatedtime} - {$a->sheettype}';
+$string['review_sheet_label'] = 'Hoja de revisión';
+$string['review_sheet_title'] = '{$a->courseshortname} - {$a->quizname} - Hoja de revisión';
+$string['showuserinfo'] = 'Identificando información para presentar acerca de usuarios';
+$string['submit_student_responses_label'] = 'Enviar respuestas...';
+$string['submit_student_responses_on_behalf'] = 'Enviar respuestas en nombre de {$a} y terminar intento';
+$string['submit_student_responses_dialog_content'] = '¿Estás seguro de enviarla?';
+$string['submit_student_responses_title'] = '{$a}: Enviar respuestas de intento';
 $string['strftime_header'] = '%d %b %Y, %H:%M';
 $string['user_identity_fields'] = ' ({$a})';
-$string['webservicecannotcreateattempts'] = 'Cannot create attempt';
+$string['webservicecannotcreateattempts'] = 'No se puede crear un intento';
 
 // Question instruction.
-$string['coderunner_instruction'] = 'Write your answer in the space provided.';
-$string['ddwtos_instruction'] = 'Write the letter of the corresponding answer (A, B, C, D, ...) in the space provided.
-After the item is the maximum number of times it can be used. e.g. (1) means that the item can be used once, (2) means twice etc. An asterisk (*) means that the items’ use is unlimited.';
-$string['ddmarker_instruction'] = 'Mark the points on the image and write the letter of corresponding answer (A, B, C, D, …) beside them.
-After the item is the maximum number of times it can be used. e.g. (1) means that the item can be used once, (2) means twice etc. An asterisk (*) means that the items’ use is unlimited.';
-$string['ddimageortext_instruction'] = 'Mark the points on the image and write the letter of corresponding answer (A, B, C, D, …) beside them.
-After the item is the maximum number of times it can be used. e.g. (1) means that the item can be used once, (2) means twice etc. An asterisk (*) means that the items’ use is unlimited.';
-$string['essay_instruction'] = 'Write your answer in the space provided.';
-$string['match_instruction'] = 'Write the letter of the corresponding answer (A, B, C, D, ...) in the space provided.';
-$string['multichoice_instruction'] = 'Select the correct answer.';
-$string['numerical_instruction'] = 'Write your answer (in numerical value) in the space provided.';
-$string['ordering_instruction'] = 'Write the correct order in the space provided.';
-$string['oumultiresponse_instruction'] = 'Select the correct answer(s).';
-$string['pmatch_instruction'] = 'Write your answer in the space provided. Please keep it to a sentence or two.';
-$string['pmatchjme_instruction'] = 'Write your answer in the space provided.';
-$string['gapselect_instruction'] = 'Write the letter of the corresponding answer (A, B, C, D, ...) in the space provided.';
-$string['shortanswer_instruction'] = 'Write your answer in the space provided. Please keep it to a sentence or two.';
-$string['stack_instruction'] = 'Write your answer in the space provided.';
-$string['truefalse_instruction'] = 'Select the correct answer.';
-$string['varnumeric_instruction'] = 'Write your answer in the space provided.';
-$string['varnumericset_instruction'] = 'Write your answer in the space provided.';
-$string['varnumunit_instruction'] = 'Write your answer in the space provided.';
-$string['wordselect_instruction'] = 'Select the answer(s) by circling the key word(s).';
+$string['coderunner_instruction'] = 'Escribe tus respuestas en el espacio proporcionado.';
+$string['ddwtos_instruction'] = 'Escribe la letra correspondiente (A, B, C, D, ...) en el espacio proporcionado.
+Luego del item está el máximo número de veces que se puede usar, por ejemplo (1) significa que el item se puede usar una vez, (2) significa dos veces, y así. Un asterisco (*) significa que el item se puede usar ilimitadamente.';
+$string['ddmarker_instruction'] = 'Marca los puntos de la imagen y escribe la letra de la respuesta correspondiente (A, B, C, D, …) junto a ellos.
+Luego del item está el máximo número de veces que se puede usar, por ejemplo (1) significa que el item se puede usar una vez, (2) significa dos veces, y así. Un asterisco (*) significa que el item se puede usar ilimitadamente.';
+$string['ddmarker_instruction'] = 'Marca los puntos de la imagen y escribe la letra de la respuesta correspondiente (A, B, C, D, …) junto a ellos.
+Luego del item está el máximo número de veces que se puede usar, por ejemplo (1) significa que el item se puede usar una vez, (2) significa dos veces, y así. Un asterisco (*) significa que el item se puede usar ilimitadamente.';
+$string['essay_instruction'] = 'Escribe tu respuesta en el espacio proporcionado.';
+$string['match_instruction'] = 'Escribe la letra de la respuesta correspondiente (A, B, C, D, ...) en el espacio proporcionado.';
+$string['multichoice_instruction'] = 'Elige la respuesta correcta.';
+$string['numerical_instruction'] = 'Escribe tu respuesta (en valores numéricos) en el espacio proporcionado.';
+$string['ordering_instruction'] = 'Escribe el orden correcto en el espacio proporcionado.';
+$string['oumultiresponse_instruction'] = 'Elige la(s) respuesta(s) correcta(s).';
+$string['pmatch_instruction'] = 'Escribe tu respuesta en el espacio proporcionado. Por favor mantenlo en una línea o dos.';
+$string['pmatchjme_instruction'] = 'Escribe tu respuesta en el espacio proporcionado.';
+$string['gapselect_instruction'] = 'Escribe la letra de la respuesta correspondiente (A, B, C, D, ...) en el espacio proporcionado.';
+$string['shortanswer_instruction'] = 'Escribe tu respuesta en el espacio proporcionado. Por favor mantenlo en una línea o dos.';
+$string['stack_instruction'] = 'Escribe tu respuesta en el espacio proporcionado.';
+$string['truefalse_instruction'] = 'Elige la respuesta correcta.';
+$string['varnumeric_instruction'] = 'Escribe tu respuesta en el espacio proporcionado.';
+$string['varnumericset_instruction'] = 'Escribe tu respuesta en el espacio proporcionado.';
+$string['varnumunit_instruction'] = 'Escribe tu respuesta en el espacio proporcionado.';
+$string['wordselect_instruction'] = 'Elige la(s) respuesta(s) encerrando en un círculo la(s) palabra(s) clave(s).';
